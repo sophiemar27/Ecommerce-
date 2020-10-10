d18fe53e53e03d7164a6d3f59a8eda8e853cd3d0
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-10-10 05:29:18 -0400

    header and footer components are added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
+    <Header />
       <h1>Welcme To ProShop</h1>
-     
+     <Footer />
       
     </>
   );
